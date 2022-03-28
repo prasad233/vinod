0ecb80e7a59d1d10dd8f6b86f62a0598f08ea652
root <root@ip-172-31-84-142.ec2.internal>
2022-03-28 13:20:37 +0000

    terraform

---
+++ prasad.tf
@@ -0,0 +1,18 @@
+provider aws {
+region = "us-east-1"
+access_key = "AKIA27BLU3ZNA6DIFO4R"
+secret_key = "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"
+}
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-bucket"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  bucket = aws_s3_bucket.b.id
+  acl    = "private"
+}
